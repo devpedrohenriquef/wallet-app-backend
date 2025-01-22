620a0f21668930b9927ed551179999fc2eec1b49
Pedro Henrique Marques do Nascimento Ferreira <pedro@MacBook-Air-de-Pedro.local>
2025-01-22 07:19:00 -0300

    feat:  implement delete category

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -2,37 +2,79 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
+const findOne = (id) => {
+  return (query = {
+    name: "fetch-category",
+    text: "SELECT * FROM categories WHERE id = $1",
+    values: [Number(id)],
+  });
+};
+
 // Endpoint para listar todas as categorias
 router.get("/", (req, res) => {
-  db.query("SELECT * FROM categories", (error, response) => {
-    if (error) {
-      return res.status(500).json(error);
-    }
-    return res.status(200).json(response.rows);
-  });
+  try {
+    db.query("SELECT * FROM categories", (error, response) => {
+      if (error) {
+        return res.status(500).json(error);
+      }
+      return res.status(200).json(response.rows);
+    });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
 
 // Endpoint para criar uma nova categoria
 router.post("/", (req, res) => {
-  const { name } = req.body;
+  try {
+    const { name } = req.body;
 
-  // Validação: nome deve ter pelo menos 3 caracteres
-  if (!name || name.length < 3) {
-    return res
-      .status(400)
-      .json({ error: "Name should have more than 3 characters" });
-  }
-
-  // Consulta SQL corrigida para inserir na tabela `categories`
-  const text = "INSERT INTO categories(name) VALUES($1) RETURNING *";
-  const values = [name];
-
-  db.query(text, values, (error, response) => {
-    if (error) {
-      return res.status(500).json(error);
+    // Validação: nome deve ter pelo menos 3 caracteres
+    if (!name || name.length < 3) {
+      return res
+        .status(400)
+        .json({ error: "Name should have more than 3 characters" });
     }
-    return res.status(200).json(response.rows[0]); // Retorna apenas o registro criado
-  });
+
+    // Consulta SQL corrigida para inserir na tabela `categories`
+    const text = "INSERT INTO categories(name) VALUES($1) RETURNING *";
+    const values = [name];
+
+    db.query(text, values, (error, response) => {
+      if (error) {
+        return res.status(500).json(error);
+      }
+      return res.status(200).json(response.rows[0]); // Retorna apenas o registro criado
+    });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({ error: "Param id is mandatory." });
+    }
+    const query = findOne(id);
+    const category = await db.query(query);
+    if (!category.rows[0]) {
+      return res.status(404).json({ error: "Category not found" });
+    }
+
+    const text = "DELETE FROM categories WHERE id =$1 RETURNING *";
+    const values = [Number(id)];
+    const deleteResponse = await db.query(text, values);
+    if (!category.rows[0]) {
+      return res.status(400).json({ error: "Category not deleted" });
+    }
+
+    return res.status(200).json(deleteResponse.rows);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
 
 module.exports = router;
