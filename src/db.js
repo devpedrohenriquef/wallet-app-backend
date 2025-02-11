7cd475f280969b7080fa0b5aad3df39654efe141
Pedro Henrique Marques do Nascimento Ferreira <pedro@MacBook-Air-de-Pedro.local>
2025-02-11 06:40:58 -0300

    chore: add dotenv and config table

--- src/db.js
+++ src/db.js
@@ -1,13 +1,16 @@
 // Importa a classe Pool do módulo 'pg' para gerenciar a conexão com o banco de dados PostgreSQL
+require("dotenv").config();
 const { Pool } = require("pg");
+
+const { DB_USER, DB_PASSWORD, DB_NAME, DB_POST, DB_PORT } = process.env;
 
 // Configuração da conexão com o banco de dados PostgreSQL
 const db = new Pool({
-  user: "docker", // Nome do usuário do banco de dados
-  password: "docker", // Senha do banco de dados
-  database: "finances", // Nome do banco de dados utilizado
-  host: "localhost", // Endereço do servidor do banco de dados
-  port: 5432, // Porta padrão do PostgreSQL
+  user: DB_USER, // Nome do usuário do banco de dados
+  password: DB_PASSWORD, // Senha do banco de dados
+  database: DB_NAME, // Nome do banco de dados utilizado
+  host: DB_POST, // Endereço do servidor do banco de dados
+  port: Number(DB_PORT), // Porta padrão do PostgreSQL
 });
 
 // Exporta a instância de conexão para ser utilizada em outras partes da aplicação
